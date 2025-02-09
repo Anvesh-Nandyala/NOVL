b637aca22434b603d421713b1fe48ed55b2df9d4
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-02-09 13:27:00 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,11 +17,11 @@ const PORT=process.env.PORT ;
 
 app.use(express.json());
 app.use("/api/users",userRoutes);
-app.use("api/auth",authRoutes);
-app.use("api/admin",adminRoutes);
-app.use("api/songs",songRoutes);
-app.use("api/albums",albumRoutes);
-app.use("api/stats",statRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/songs",songRoutes);
+app.use("/api/albums",albumRoutes);
+app.use("/api/stats",statRoutes);
 
 
 
